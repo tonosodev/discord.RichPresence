e5874a61324b44c1688103612fbd757a3d198428
Alexey Shvetsov <DevilDesigner@users.noreply.github.com>
2021-02-20 21:57:49 +0600

    Some changes

--- run.py
+++ run.py
@@ -11,26 +11,32 @@ try:
     RPC = Presence(client_id)  # Initialize the Presence class
     RPC.connect()
     print("Connected!")
-    quotes_button_1 = ["Contact me!", "VKONTAKTE", "VK.COM"]
-    quotes_button_2 = [""]
+    quotes_button_1 = ["Have a question?...", "Contact me!", "¸,ø¤º°`°º¤ø,¸ 𝕍𝕂 ¸,ø¤º°`°º¤ø,¸"]
+    quotes_button_2 = ["Join to Meta Peace Team®", "My Discord Server", "Yukki is waiting for you!"]
+
+    quotes_details = [""]
+    quotes_state = [""]
+
     quotes_small_text = ["In Code We Trust", "Contact me!"]
     quotes_large_text = ["Meooow! 💜", "What do u think about that?"]
-    quotes_large_image = ["cat_img_1", "cat_img_2", "cat_img_3"]
+    quotes_large_image = ["cat1_1024x1024", "cat2_1024x1024", "cat3_1024x1024"]
     while True:
         cpu_per = round(psutil.cpu_percent(), 1)
         mem = psutil.virtual_memory()
         buttons_list = [{"label": random.choice(quotes_button_1), "url": "https://vk.com/devildesigner"},
-                        {"label": "| GitHub |", "url": "https://github.com/DevilDesigner"},
+                        {"label": random.choice(quotes_button_2), "url": "https://discord.gg/ZrfkCEAcfW"},
                         ]
         mem_per = round(psutil.virtual_memory().percent, 1)
 
+
         RPC.update(
             _donotuse=True,
-            start=1,
+            # start=time(),
             large_text=random.choice(quotes_large_text),
             small_text=random.choice(quotes_small_text),
             large_image=random.choice(quotes_large_image),
             small_image="profile_image",
+
             # party_size=[1, 4],
             buttons=buttons_list,
             details="RAM: " + str(mem_per) + "%",
