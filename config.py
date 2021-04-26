82a7024c4deb0663a2264ee4e81dac615b792fed
Alexey Shvetsov <59541735+DevilDesigner@users.noreply.github.com>
2021-04-27 00:10:52 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 general_settings = {
-    "client_id": "xxxxxxxxxxxxxxxxxxx",   # Our Discord Bot clientID
+    "client_id": "xxxxxxxxxxxxxxxxxxx",   # Your Discord Bot clientID
     "next_layer_time": 10,                # Active layer time
     "loading_time": 5,                    # Re-loading connection time, if Discord don't runned or Ethernet connection dropped
 }
