5de091293b1f7ab0882ec95fe82799650c6fb8d9
Alexey Shvetsov <DevilDesigner@users.noreply.github.com>
2021-07-18 06:00:37 +0600

    Some changes

---
+++ config.py
@@ -0,0 +1,21 @@
+# Layers configuration
+# You can use <["text1", "text2", "..."]> structure for randomize activity status or <["text"]> structure for single text!
+# Contact me (https://vk.com/devildesigner) if you have some troubles.
+
+# Buttons
+first_button_layer_1_text = ["Layer 1 first Button Text"]
+first_button_layer_1_url = ["https://first_button_url_here"]
+second_button_layer_1_text = ["Layer 1 second Button Text"]
+second_button_layer_1_url = ["https://second_button_url_here"]
+first_button_layer_2_text = ["Layer 2 first Button Text"]
+first_button_layer_2_url = ["https://discord.gg/YkyN4ws8C9"]
+# LayersQuotes
+quotes_large_image_text = ["Large Image text!"]
+quotes_small_image_text = ["Small Image text!"]
+window_error_large_text = ["#404: Problems Not Found."]
+window_error_small_text = ["Small Image text!"]
+# LayerImages
+quotes_large_image = ["your_image_1_name", "your_image_2_name"]
+quotes_small_image = ["your_image_3_name"]
+window_error_large_image = ["your_image_4_name"]
+window_error_small_image = ["your_image_5_name"]
